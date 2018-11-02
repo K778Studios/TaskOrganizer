8759c6a7d5e1f7e9ced3bf6b7356f640d6845c2c
mdolinski <mdolinski@mdolinski-VirtualBox>
2018-11-01 20:21:09 -0400

    Initial commit of TaskOrganizer project

---
+++ TaskOrganizer/src/main/java/com/k778studios/TaskOrganizer/TaskOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.k778studios.TaskOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskOrganizerApplication.class, args);
+		System.out.println("Hello World");
+	}
+}
