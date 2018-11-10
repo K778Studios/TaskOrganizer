3f7634bd3e5f60500021f50c0da3bbe8fd39a7fd
mdolinski <mdolinski@mdolinski-VirtualBox>
2018-11-10 13:38:06 -0500

    - Simple Test Page that tests out the MVC capabilities, and changes to the dependencies in order to run the project

--- TaskOrganizer/src/main/java/com/k778studios/TaskOrganizer/controller/SimpleController.java
+++ TaskOrganizer/src/main/java/com/k778studios/TaskOrganizer/controller/SimpleController.java
@@ -1,16 +1,19 @@
 package com.k778studios.TaskOrganizer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SimpleController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String getIndexPage()
+	@RequestMapping({"/", ""})
+	public String getIndexPage(Model model)
 	{
-		return "/";
+		
+		model.addAttribute("helloWorld", "Hello World");
+		
+		return "/simplePage";
 	}
 	
 }
