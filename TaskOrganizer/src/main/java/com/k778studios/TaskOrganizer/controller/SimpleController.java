6131fae21321510f1d23706936ffb150d0c25502
mdolinski <mdolinski@mdolinski-VirtualBox>
2018-11-10 15:32:03 -0500

    Update the background image

--- TaskOrganizer/src/main/java/com/k778studios/TaskOrganizer/controller/SimpleController.java
+++ TaskOrganizer/src/main/java/com/k778studios/TaskOrganizer/controller/SimpleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SimpleController {
 
-	@RequestMapping({"/", ""})
+	@RequestMapping("/simplePage")
 	public String getIndexPage(Model model)
 	{
 		
